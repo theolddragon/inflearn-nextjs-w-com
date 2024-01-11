eec12424a00e7868c8ac40ae20a6273047b07c07
tigger <tigger@cherground.com>
2024-01-11 17:05:43 +0900

    Session 2. Explore UI 작성

--- src/app/(afterLogin)/explore/page.tsx
+++ src/app/(afterLogin)/explore/page.tsx
@@ -1,6 +1,26 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import SearchForm from "@/app/(afterLogin)/_component/SearchForm";
+import style from './explore.module.css'
+import Trend from "@/app/(afterLogin)/_component/Trend";
 
-export default function Home() {
-  return null
+export default function Page() {
+  return (
+      <main className={style.main}>
+          <div className={style.formZone}>
+              <SearchForm/>
+          </div>
+          <div className={style.trend}>
+              <h3>나를 위한 트렌드</h3>
+              <Trend />
+              <Trend />
+              <Trend />
+              <Trend />
+              <Trend />
+              <Trend />
+              <Trend />
+              <Trend />
+              <Trend />
+              <Trend />
+          </div>
+      </main>
+  )
 }
