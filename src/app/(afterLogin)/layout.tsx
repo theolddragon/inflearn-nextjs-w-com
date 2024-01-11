72f1e596db5783ec13c03f2c6db49f0c209c20e0
tigger <tigger@cherground.com>
2024-01-11 15:16:05 +0900

    Session 2. Layout - Right Section UI 작성

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -4,8 +4,10 @@ import styles from './layout.module.css'
 import Link from "next/link";
 import Image from 'next/image'
 import ZLogo from '/public/zlogo.png'
-import NavMenu from './_component/NavMenu';
+import NavMenu from '@/app/(afterLogin)/_component/NavMenu';
 import LogoutButton from "@/app/(afterLogin)/_component/LogoutButton";
+import FollowRecommend from "@/app/(afterLogin)/_component/FollowRecommend";
+import TrendSection from "@/app/(afterLogin)/_component/TrendSection";
 
 const AfterLoginLayout = async ({children}: {
     children: React.ReactNode
@@ -34,15 +36,24 @@ const AfterLoginLayout = async ({children}: {
                 <div className={styles.rightSectionInner}>
                     <main className={styles.main}>{children}</main>
                     <section className={styles.rightSection}>
-                        <form action="" className={styles.search}>
-                            <svg width={20} viewBox="0 0 24 24" aria-hidden="true">
-                                <g>
-                                    <path
-                                        d="M10.25 3.75c-3.59 0-6.5 2.91-6.5 6.5s2.91 6.5 6.5 6.5c1.795 0 3.419-.726 4.596-1.904 1.178-1.177 1.904-2.801 1.904-4.596 0-3.59-2.91-6.5-6.5-6.5zm-8.5 6.5c0-4.694 3.806-8.5 8.5-8.5s8.5 3.806 8.5 8.5c0 1.986-.682 3.815-1.824 5.262l4.781 4.781-1.414 1.414-4.781-4.781c-1.447 1.142-3.276 1.824-5.262 1.824-4.694 0-8.5-3.806-8.5-8.5z"></path>
-                                </g>
-                            </svg>
-                            <input type="search"/>
-                        </form>
+                        <div style={{marginBottom: 60, width: 'inherit'}}>
+                            <form action="" className={styles.search}>
+                                <svg width={20} viewBox="0 0 24 24" aria-hidden="true">
+                                    <g>
+                                        <path
+                                            d="M10.25 3.75c-3.59 0-6.5 2.91-6.5 6.5s2.91 6.5 6.5 6.5c1.795 0 3.419-.726 4.596-1.904 1.178-1.177 1.904-2.801 1.904-4.596 0-3.59-2.91-6.5-6.5-6.5zm-8.5 6.5c0-4.694 3.806-8.5 8.5-8.5s8.5 3.806 8.5 8.5c0 1.986-.682 3.815-1.824 5.262l4.781 4.781-1.414 1.414-4.781-4.781c-1.447 1.142-3.276 1.824-5.262 1.824-4.694 0-8.5-3.806-8.5-8.5z"></path>
+                                    </g>
+                                </svg>
+                                <input type="search"/>
+                            </form>
+                        </div>
+                        <TrendSection/>
+                        <div className={styles.followRecommend}>
+                            <h3>팔로우 추천</h3>
+                            <FollowRecommend/>
+                            <FollowRecommend/>
+                            <FollowRecommend/>
+                        </div>
                     </section>
                 </div>
             </div>
