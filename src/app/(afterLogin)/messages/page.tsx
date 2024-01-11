88f5b38f976bfd8f3530b93dfaa203a8f6a1bed7
tigger <tigger@cherground.com>
2024-01-11 18:43:48 +0900

    Session 2. Messages UI 작성

--- src/app/(afterLogin)/messages/page.tsx
+++ src/app/(afterLogin)/messages/page.tsx
@@ -1,6 +1,18 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import style from './message.module.css';
+import Room from "@/app/(afterLogin)/messages/_component/Room";
 
-export default function Home() {
-  return null
+export default function Messages() {
+  return (
+      <main className={style.main}>
+        <div className={style.header}>
+          <h3>쪽지</h3>
+        </div>
+        <Room/>
+        <Room/>
+        <Room/>
+        <Room/>
+        <Room/>
+        <Room/>
+      </main>
+  )
 }
