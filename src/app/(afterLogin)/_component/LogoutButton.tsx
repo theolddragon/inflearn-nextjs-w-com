aeca53c8c01d2e04b9332e5df9dfa1d6d7f59cb3
tigger <tigger@cherground.com>
2024-01-15 18:13:19 +0900

    feat: next-auth logout

--- src/app/(afterLogin)/_component/LogoutButton.tsx
+++ src/app/(afterLogin)/_component/LogoutButton.tsx
@@ -1,15 +1,29 @@
 "use client"
 
 import styles from "./logoutButton.module.css";
+import { useRouter } from 'next/navigation'
+import { signOut } from 'next-auth/react'
 
 export default function LogoutButton() {
-    const me = { // 임시로 내 정보 있는것처럼
+  const router = useRouter()
+  const me = { // 임시로 내 정보 있는것처럼
         id: 'wanderhoward',
         nickname: '떠돌이호랑이',
         image: '/5Udwvqim.jpg',
     }
 
-    const onLogout = () => {};
+    const onLogout = () => {
+      signOut( {
+        redirect: false
+      })
+      .then((response) => {
+        console.log(`response:${response}`)
+        router.replace("/")
+      })
+      .catch((error) => {
+        console.log(`error:${error}`)
+      })
+    };
 
     return (
         <button className={styles.logOutButton} onClick={onLogout}>
